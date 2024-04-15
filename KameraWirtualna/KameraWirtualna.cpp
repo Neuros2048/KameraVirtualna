537ee2325073700b2d0b8330f5a786c246668b8a
Neuros2048 <95345778+Neuros2048@users.noreply.github.com>
2024-04-15 19:22:25 +0200

    dziłające bsptree

--- KameraWirtualna/KameraWirtualna.cpp
+++ KameraWirtualna/KameraWirtualna.cpp
@@ -22,7 +22,50 @@ int height = 800;
 struct Point {
     double x, y, z;
 };
+struct Wall {
+    Point point1, point2, point3, point4;
+};
+bool isPointBehindWall(Point p1, Point p2, const Wall& wall) {
+    // Iteracja przez wszystkie boki ściany
+    for (int i = 0; i < 4; ++i) {
+        Point p3, p4;
+        // Ustawienie punktów p3 i p4 na kolejne boki ściany
+        switch (i) {
+        case 0:
+            p3 = wall.point1;
+            p4 = wall.point2;
+            break;
+        case 1:
+            p3 = wall.point2;
+            p4 = wall.point3;
+            break;
+        case 2:
+            p3 = wall.point3;
+            p4 = wall.point4;
+            break;
+        case 3:
+            p3 = wall.point4;
+            p4 = wall.point1;
+            break;
+        }
+        
+        // Wektor normalny do płaszczyzny ściany
+        double normal_x = (p4.y - p3.y) * (p1.z - p3.z) - (p4.z - p3.z) * (p1.y - p3.y);
+        double normal_y = (p4.z - p3.z) * (p1.x - p3.x) - (p4.x - p3.x) * (p1.z - p3.z);
+        double normal_z = (p4.x - p3.x) * (p1.y - p3.y) - (p4.y - p3.y) * (p1.x - p3.x);
 
+        // Wektor od punktu p2 do punktu p1
+        double vector_x = p1.x - p2.x;
+        double vector_y = p1.y - p2.y;
+        double vector_z = p1.z - p2.z;
+
+        // Sprawdzenie czy punkt p1 jest po przeciwnej stronie ściany względem punktu p2
+        if (normal_x * vector_x + normal_y * vector_y + normal_z * vector_z > 0) {
+            return false;
+        }
+    }
+    return true;
+}
 // Funkcja obliczająca wektor normalny do płaszczyzny na podstawie trzech punktów
 void calculateNormal(const Point& p1, const Point& p2, const Point& p3, double& a, double& b, double& c) {
     // Obliczenie dwóch wektorów tworzących trójkąt
@@ -48,15 +91,12 @@ void calculateNormal(const Point& p1, const Point& p2, const Point& p3, double& 
     }
 }
 
-// Funkcja wyznaczająca równanie płaszczyzny na podstawie wektora normalnego i punktu na płaszczyźnie
 double planeEquation(double a, double b, double c, const Point& point) {
     double d = -(a * point.x + b * point.y + c * point.z); // Obliczenie odległości od początku układu współrzędnych
     return d/ sqrt(a * a + b * b + c * c);
     // Wyświetlenie równania płaszczyzny
     
 }
-
-
 
 wieszcholek * maketree(wieszcholek * parent, wieszcholek x)
 {
@@ -69,23 +109,27 @@ wieszcholek * maketree(wieszcholek * parent, wieszcholek x)
         nowy->color = x.color;
         return nowy;
     }
-    double z1max = 0,z2max = 0;
+    double z1max = 0,z2max = 0,z1min = INFINITY ,z2min =INFINITY;
     Point p1 = {parent->pt[parent->wall[0]][0], parent->pt[parent->wall[0]][1], parent->pt[parent->wall[0]][2]};
     Point p2 = {parent->pt[parent->wall[1]][0], parent->pt[parent->wall[1]][1], parent->pt[parent->wall[1]][2]};
     Point p3 = {parent->pt[parent->wall[2]][0], parent->pt[parent->wall[2]][1], parent->pt[parent->wall[2]][2]};
-
-    // Wyznaczenie wektora normalnego do płaszczyzny
-    double a, b, c;
+    Point p4 = {parent->pt[parent->wall[3]][0], parent->pt[parent->wall[3]][1], parent->pt[parent->wall[3]][2]};
+    Wall ww {p1,p2,p3,p4};
+    double a, b, c,d,t;
     calculateNormal(p1, p2, p3, a, b, c);
-    // Użycie jednego z punktów do wyznaczenia równania płaszczyzny
-    z1max = planeEquation(a, b, c, p1);
-
-     p1 = {x.pt[x.wall[0]][0], x.pt[x.wall[0]][1], x.pt[x.wall[0]][2]};
-     p2 = {x.pt[x.wall[1]][0], x.pt[x.wall[1]][1], x.pt[x.wall[1]][2]};
-     p3 = {x.pt[x.wall[2]][0], x.pt[x.wall[2]][1], x.pt[x.wall[2]][2]};
-    calculateNormal(p1, p2, p3, a, b, c);
-    z2max = planeEquation(a, b, c, p1);
-    if(z2max > z1max)
+    d = planeEquation(a, b, c, p4);
+    
+    t = ((x.pt[x.wall[0]][0]+x.pt[x.wall[1]][0]+x.pt[x.wall[2]][0]+x.pt[x.wall[3]][0])/4*a + (x.pt[x.wall[0]][1]+x.pt[x.wall[1]][1]+x.pt[x.wall[2]][1]+x.pt[x.wall[3]][1])/4* b+(x.pt[x.wall[0]][2]+x.pt[x.wall[1]][2]+x.pt[x.wall[2]][2]+x.pt[x.wall[3]][2])/4*c )/-d;
+    
+    for(int i = 0 ; i < 4;i++)
+    {
+        z1max = max(z1max,  parent->pt[ parent->wall[i] ][2]);
+        z2max = max(z2max,  x.pt[ x.wall[i] ][2]);
+        z1min = min(z1min,  parent->pt[ parent->wall[i] ][2]);
+        z2min = min(z2min,  x.pt[ x.wall[i] ][2]);
+    }
+    
+    if(t>=1)
     {
         parent->left =  maketree(parent->left, x);
     }else
@@ -395,54 +439,7 @@ int main()
                     czworoscian.setPoint(2,sf::Vector2f(s3*p3[0] + width/2,-s3*p3[1] + height/2));
                     czworoscian.setPoint(3,sf::Vector2f(s4*p4[0] + width/2,-s4*p4[1] + height/2));
                     czworoscian.setFillColor(curr.first->color);
-                   /* if(p1[2]<=0)
-                    {
-                        z1 = p1[2];
-                        z2 = p2[2];
-                        x1 = (p2[0]- p1[0])* (z2/(z2-z1));
-                        x2 =  p2[0] -x1;
-                        y1 = (p2[1]- p1[1])* (z2/(z2-z1));
-                        y2 =  p2[1] -y1;
-                        x2 = abs(x2)<0.1? 1:x2;
-                        y2 = abs(y2)<0.1? 1:y2;
-                        czworoscian.setPoint(0,sf::Vector2f(10.0*ct*x2 + width/2,-10.0*ct*y2 + height/2));
-                    }
-                    if(p2[2]<=0)
-                    {
-                        z1 = p2[2];
-                        z2 = p3[2];
-                        x1 = (p3[0]- p2[0])* (z2/(z2-z1));
-                        x2 =  p3[0] -x1;
-                        y1 = (p3[1]- p2[1])* (z2/(z2-z1));
-                        y2 =  p3[1] -y1;
-                        x2 = abs(x2)<0.1? 1:x2;
-                        y2 = abs(y2)<0.1? 1:y2;
-                        czworoscian.setPoint(0,sf::Vector2f(10.0*ct*x2 + width/2,-10.0*ct*y2 + height/2));
-                    }
-                    if(p3[2]<=0)
-                    {
-                        z1 = p3[2];
-                        z2 = p4[2];
-                        x1 = (p4[0]- p3[0])* (z2/(z2-z1));
-                        x2 =  p4[0] -x1;
-                        y1 = (p4[1]- p3[1])* (z2/(z2-z1));
-                        y2 =  p4[1] -y1;
-                        x2 = abs(x2)<0.1? 1:x2;
-                        y2 = abs(y2)<0.1? 1:y2;
-                        czworoscian.setPoint(0,sf::Vector2f(10.0*ct*x2 + width/2,-10.0*ct*y2 + height/2));
-                    }
-                    if(p4[2]<=0)
-                    {
-                        z1 = p4[2];
-                        z2 = p1[2];
-                        x1 = (p1[0]- p4[0])* (z2/(z2-z1));
-                        x2 =  p1[0] -x1;
-                        y1 = (p1[1]- p4[1])* (z2/(z2-z1));
-                        y2 =  p1[1] -y1;
-                        x2 = abs(x2)<0.1? 1:x2;
-                        y2 = abs(y2)<0.1? 1:y2;
-                        czworoscian.setPoint(0,sf::Vector2f(10.0*ct*x2 + width/2,-10.0*ct*y2 + height/2));
-                    }*/
+                   
                     window.draw(czworoscian);
                 }
             }
